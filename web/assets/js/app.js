be65eb36733875817b5a08710c765154ace8397f
Katamaranas <ernestas.zidokas@gmail.com>
2019-05-22 16:24:29 +0300

    added functionality to remove fullscreen on click, started creating carousel

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,7 +1,6 @@
 const photoArray = [{link: 'banana.gif', fullscreen: false}, {link: 'bananas.jpg', fullscreen: false}, {link: 'friends.gif', fullscreen: false}, {link: 'noballs.jpg', fullscreen: false}, {link: 'tits.gif', fullscreen: false}];
 
 window.addEventListener('load', (event)=>{
-  clearGallery();
   createGallery();
 });
 
@@ -10,20 +9,52 @@ function createGallery() {
     let div = document.createElement('div');
     let img = document.createElement('img');
     img.src = 'assets/images/' + photo.link;
-    if(!photo.fullscreen){
-      img.classList.add('photo');
-    } else {
-      img.classList.add('fullscreen');
-    }
+    img.classList.add('photo');
 
     img.addEventListener('click', event => {
       photo.fullscreen = !photo.fullscreen;
-      clearGallery();
-      createGallery();
+
+      if(photo.fullscreen){
+        let divFullScreen = document.createElement('div');
+        divFullScreen.classList.add('fullscreen-div');
+
+        let imgFullScreen = document.createElement('img');
+        imgFullScreen.src = 'assets/images/' + photo.link;
+        imgFullScreen.classList.add('fullscreen');
+
+        let arrowLeft = document.createElement('div');
+        arrowLeft.classList.add('arrow-left');
+        arrowLeft.addEventListener('click', event => {
+          console.log('kaire');
+        })
+
+        let arrowRight = document.createElement('div');
+        arrowRight.classList.add('arrow-right');
+
+        arrowRight.addEventListener('click', event => {
+          console.log('desine');
+          document.querySelector('.fullscreen-div').removeChild(document.querySelector('.fullscreen'));
+          let imgFullScreen = document.createElement('img');
+          index++;
+          imgFullScreen.src = 'assets/images/' + photoArray[index].link;
+          imgFullScreen.classList.add('fullscreen');
+          divFullScreen.appendChild(imgFullScreen);
+        })
+
+        imgFullScreen.addEventListener('click', event => {
+          photo.fullscreen = !photo.fullscreen;
+          document.querySelector('#gallery').removeChild(divFullScreen);
+        })
+
+        divFullScreen.appendChild(arrowLeft);
+        divFullScreen.appendChild(imgFullScreen);
+        divFullScreen.appendChild(arrowRight);
+        document.querySelector('#gallery').appendChild(divFullScreen);
+      }
     });
 
     div.appendChild(img);
-    document.querySelector('#gallery').appendChild(div)
+    document.querySelector('#gallery').appendChild(div);
   });
 }
 
